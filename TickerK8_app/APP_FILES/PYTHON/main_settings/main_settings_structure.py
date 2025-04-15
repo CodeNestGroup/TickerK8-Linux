710c12b46cab450f3d20e6bde993a19a2559f7f7
bartosz <bartosz_magister@protonmail.com>
2025-04-15 23:55:51 +0200

    v 0.0.3.12

--- TickerK8_app/APP_FILES/PYTHON/main_settings/main_settings_structure.py
+++ TickerK8_app/APP_FILES/PYTHON/main_settings/main_settings_structure.py
@@ -8,7 +8,6 @@ from PyQt5.QtWidgets import (
     QScrollArea, # Simple scroll widget 
     QLabel, # Simple label
     QPushButton, # Simple button
-    QLineEdit, # Simple line edit
     QComboBox, # Drop down list
     QGridLayout, # Grid layout
     QVboxLayout # Vertical layout 
@@ -45,7 +44,7 @@ class Main_settings_widget(QWidget):
         self.navi_update_button = QPushButton(self.navi_widget)
         self.navi_language_button = QPushButton(self.navi_widget)
         self.navi_report_button = QPushButton(self.navi_widget)
-        self.navi_exit_button  = QPushButton(self.main_layout)
+        self.navi_exit_button  = QPushButton(self)
         self.panel_right_scroll = QScrollArea(self)
         self.panel_right_widget = QWidget(self.panel_right_scroll)
         self.panel_right_layout = QVboxLayout(self.panel_right_widget)
