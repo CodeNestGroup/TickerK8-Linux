c4426048413487579a758d22d9e91d496f5e6afd
Bartosz Brudny <bartosz_magister@protonmail.com>
2025-07-30 17:19:00 +0200

    v 0.0.3.67

--- TickerK8_app/APP_FILES/PYTHON/main_search/main_search_ui.py
+++ TickerK8_app/APP_FILES/PYTHON/main_search/main_search_ui.py
@@ -13,15 +13,18 @@ from PyQt5.QtCore import (
 #______________________________________________________________________________________________________________________
 """ Import PyQt5 Gui """
 from PyQt5.QtGui import (
-    QIcon # Icon
+    QIcon, # Icon
+    QPixmap
 )
+#_______________________________________________________________________________________________________________________
+""" Import PyQt5 Svg """
+from PyQt5.QtSvg import QSvgRenderer # Render Svg.
 #######################################################################################################################
 """ Main search Ui """
 def main_search_ui(self):
     """ Set object name """
     self.setObjectName('main_search_widget')
     self.panel_widget.setObjectName('panel_widget')
-    self.panel_search_icon_label.setObjectName('panel_search_icon_label')
     self.panel_search_lineedit.setObjectName('panel_search_lineedit')
     self.panel_type_stock_button.setObjectName('panel_type_stock_button')
     self.panel_type_etf_button.setObjectName('panel_type_etf_button')
@@ -29,9 +32,9 @@ def main_search_ui(self):
     self.panel_type_index_button.setObjectName('panel_type_index_button')
     self.panel_type_market_button.setObjectName('panel_type_market_button')
     self.panel_type_country_button.setObjectName('panel_type_country_button')
-    self.panel_sort_id_button.setObjectName('panel_sort_id_button')
-    self.panel_logo_label.setObjectName('panel_logo_label')
-    self.panel_sort_name_button.setObjectName('panel_sort_name_button')
+    self.panel_id_label.setObjectName('panel_id_label')
+    self.panel_object_name_label.setObjectName('panel_object_name_label')
+    self.panel_market_name_label.setObjectName('panel_market_name_label')
     self.panel_scroll.setObjectName('panel_scroll')
     self.panel_exit_button.setObjectName('panel_exit_button')
 #______________________________________________________________________________________________________________________
@@ -42,8 +45,9 @@ def main_search_ui(self):
     self.panel_type_index_button.setProperty('class', 'panel_type_buttons')
     self.panel_type_market_button.setProperty('class', 'panel_type_buttons')
     self.panel_type_country_button.setProperty('class', 'panel_type_buttons')
-    self.panel_sort_id_button.setProperty('class', 'panel_sort_buttons')
-    self.panel_sort_name_button.setProperty('class', 'panel_sort_buttons')
+    self.panel_id_label.setProperty('class', 'panel_tags')
+    self.panel_object_name_label.setProperty('class', 'panel_tags')
+    self.panel_market_name_label.setProperty('class', 'panel_tags')
 #______________________________________________________________________________________________________________________
     """ Set layout """
     self.main_layout.addWidget(self.panel_widget, 10, 20, 80, 60)
@@ -53,19 +57,18 @@ def main_search_ui(self):
         self.main_layout.setRowStretch(enc, 1)
         self.main_layout.setColumnStretch(enc, 1)
     self.setLayout(self.main_layout)
-    self.panel_layout.addWidget(self.panel_search_icon_label, 5, 35, 5, 5)
-    self.panel_layout.addWidget(self.panel_search_lineedit, 5, 40, 5, 20)
+    self.panel_layout.addWidget(self.panel_search_lineedit, 5, 30, 5, 40)
     self.panel_layout.addWidget(self.panel_type_stock_button, 15, 5, 3, 14)
-    self.panel_layout.addWidget(self.panel_type_etf_button, 15, 21, 3, 14)
-    self.panel_layout.addWidget(self.panel_type_forex_button, 15, 37, 3, 14)
-    self.panel_layout.addWidget(self.panel_type_index_button, 15, 53, 3, 14)
-    self.panel_layout.addWidget(self.panel_type_market_button, 15, 69, 3, 14)
-    self.panel_layout.addWidget(self.panel_type_country_button, 15, 85, 3, 14)
-    self.panel_layout.addWidget(self.panel_sort_id_button, 20, 5, 3, 5)
-    self.panel_layout.addWidget(self.panel_logo_label, 20, 10, 3, 5)
-    self.panel_layout.addWidget(self.panel_sort_name_button, 20, 15, 3, 5)
+    self.panel_layout.addWidget(self.panel_type_etf_button, 15, 20, 3, 14)
+    self.panel_layout.addWidget(self.panel_type_forex_button, 15, 35, 3, 14)
+    self.panel_layout.addWidget(self.panel_type_index_button, 15, 50, 3, 14)
+    self.panel_layout.addWidget(self.panel_type_market_button, 15, 65, 3, 14)
+    self.panel_layout.addWidget(self.panel_type_country_button, 15, 80, 3, 14)
+    self.panel_layout.addWidget(self.panel_id_label, 20, 7, 3, 5)
+    self.panel_layout.addWidget(self.panel_object_name_label, 20, 26, 3, 26)
+    self.panel_layout.addWidget(self.panel_market_name_label, 20, 66, 3, 26)
     self.panel_layout.addWidget(self.panel_scroll, 23, 5, 67, 90)
-    self.panel_layout.addWidget(self.panel_exit_button, 93, 40, 4, 20)
+    self.panel_layout.addWidget(self.panel_exit_button, 2, 5, 6, 5)
     self.panel_layout.setSpacing(0)
     self.panel_layout.setContentsMargins(0,0,0,0)
     for enc in range(100):
@@ -77,8 +80,7 @@ def main_search_ui(self):
     self.setHidden(False)
 #______________________________________________________________________________________________________________________
     """ Set label """
-    self.panel_search_icon_label.setAlignment(Qt.AlignCenter)
-    self.panel_logo_label.setAlignment(Qt.AlignCenter)
+    self.panel_id_label.setAlignment(Qt.AlignCenter)
 #______________________________________________________________________________________________________________________
     """ Set line edit """
 #______________________________________________________________________________________________________________________
@@ -86,7 +88,6 @@ def main_search_ui(self):
     self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     self.setGeometry(0, 0, self.parent.width(), self.parent.height())
     self.panel_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-    self.panel_search_icon_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     self.panel_search_lineedit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     self.panel_type_stock_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     self.panel_type_etf_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -94,15 +95,16 @@ def main_search_ui(self):
     self.panel_type_index_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     self.panel_type_market_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     self.panel_type_country_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-    self.panel_sort_id_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-    self.panel_logo_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-    self.panel_sort_name_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+    self.panel_id_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+    self.panel_object_name_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+    self.panel_market_name_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     self.panel_scroll.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
     self.panel_exit_button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 #######################################################################################################################
 """ Main search style """
 def main_search_reload_style(self):
     self.setStyleSheet(open(str(self.main_path+'/STYLE/CSS/main_search/'+self.global_config['__theme__']+'.css')).read())
+    self.panel_exit_button.setIcon(QIcon(load_svg(self.main_path+'/STYLE/IMG/icons/main/exit_'+self.global_config['__theme__']+'.svg', 256, 256)))
 #######################################################################################################################
 """ Main search retranslate """
 def main_search_retranslate(self):
@@ -115,8 +117,19 @@ def main_search_retranslate(self):
     self.panel_type_index_button.setText(_t['panel_type_index_button'][_l])
     self.panel_type_market_button.setText(_t['panel_type_index_button'][_l])
     self.panel_type_country_button.setText(_t['panel_type_country_button'][_l])
-    self.panel_sort_id_button.setText(_t['panel_sort_id_button'][_l])
-    self.panel_logo_label.setText(_t['panel_logo_label'][_l])
-    self.panel_sort_name_button.setText(_t['panel_sort_name_button'][_l])
-    self.panel_exit_button.setText(_t['panel_exit_button'][_l])
+    self.panel_id_label.setText('#')
+    self.panel_object_name_label.setText(_t['panel_object_name_label'][_l])
+    self.panel_market_name_label.setText(_t['panel_market_name_label'][_l])
 #######################################################################################################################
+""" Load svg script """
+def load_svg(svg_path, width, height):
+    renderer = QSvgRenderer(svg_path) # Render svg
+    pixmap = QPixmap(width, height) # Create pixmap
+    pixmap.fill(Qt.transparent) # Transparent
+    painter = QPainter(pixmap) # Render graphic 
+    renderer.render(painter) # Render graphic
+    painter.end() # Render graphic
+    scaled_pixmap = pixmap.scaled(QSize(width, height), Qt.KeepAspectRatio, Qt.SmoothTransformation) # Scal pixmap
+    return scaled_pixmap
+#######################################################################################################################
+
