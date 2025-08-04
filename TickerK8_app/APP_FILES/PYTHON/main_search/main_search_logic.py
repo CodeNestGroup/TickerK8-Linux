229b73cf15570ebb934ae81527e763f72ebba1f9
Bartosz Brudny <bartosz_magister@protonmail.com>
2025-08-04 12:20:19 +0200

    v 0.0.3.69

--- TickerK8_app/APP_FILES/PYTHON/main_search/main_search_logic.py
+++ TickerK8_app/APP_FILES/PYTHON/main_search/main_search_logic.py
@@ -6,11 +6,55 @@ def load_history(self):
 def text_changed(self):
     _text = self.panel_search_lineedit.text() # Get searching text
     _active_filters = self.global_config['main_search_filters'] # Get filters 
+#______________________________________________________________________________________________________________________
+    """ Get data """
     database = sqlite3.connect(database=self.main_path+'/CONFIG/GLOBAL/local_data_prototype.db') # Create connect 
-    cursor = database.cursor() # Create cursor 
-    country_data = cursor.execute(f'''
-    SELECT FROM WHERE name like {_text};
-    ''')
+    cursor = database.cursor() # Create cursor
+    all_data = []
+    if _active_filters[0]:
+        all_data.append(cursor.execute(f'''
+            SELECT logo, name, logo, name FROM stock WHERE name like {_text};
+        '''))
+    elif _active_filters[1]:
+        all_data.append(cursor.execute(f'''
+            SELECT logo, name, logo, name FROM etf WHERE name like {_text};
+        '''))
+    elif _active_filters[2]:
+        all_data.append(cursor.execute(f'''
+            SELECT logo, name, logo, name FROM WHERE name like {_text};
+        '''))
+    elif _active_filters[3]:
+        all_data.append(cursor.execute(f'''
+            SELECT logo, name, logo, name FROM WHERE name like {_text};
+        '''))
+    elif _active_filters[4]: 
+        all_data.append(cursor.execute(f'''
+            SELECT logo, name FROM WHERE name like {_text};
+        '''))
+    elif _active_filters[5]:
+        all_data.append(cursor.execute(f'''
+            SELECT icon, name FROM country WHERE name like %{_text}%;
+        '''))
+#______________________________________________________________________________________________________________________
+    """ Set up """
+    if self.panel_scroll_widget:
+        self.panel_scroll_widget.deleteLater()
+        self.panel_scroll_widget = None 
+    """ Create widget """
+    self.panel_scroll_widget = QWidget(self.panel_scroll)
+    self.panel_scroll_widget.setObjectName('panel_scroll_widget')
+    self.panel_scroll.setWidget(self.panel_scroll_widget)
+    self.panel_scroll_layout = QGridLayout(self.panel_scroll_widget)
+    self.panel_scroll_layout.setSpacing(0)
+    self.panel_scroll_layout.setContentsMargins(0,0,0,0)
+    self.panel_scroll_widget.setLayout(self.panel_scroll_layout)
+    for list_object in enumerate(all_data, start=1):
+        index_label = QLabel(self.panel_widget)
+        object_logo_label = QLabel(self.panel_widget)
+        object_name_button = QPushButton(self.panel_widget)
+        market_logo_label = QLabel(self.panel_widget)
+        market_name_label = QLabel(self.panel_widget)
+
 
 #######################################################################################################################
 def filters_changed(self, index):
