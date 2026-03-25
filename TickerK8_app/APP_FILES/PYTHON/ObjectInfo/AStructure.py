b1e8c3c23f96b20bcdcd7cf1a980d4cdfa269eb8
Bartosz Brudny <bartosz_magister@protonmail.com>
2026-03-25 23:43:24 +0100

    Updating Main widget save31

--- TickerK8_app/APP_FILES/PYTHON/ObjectInfo/AStructure.py
+++ TickerK8_app/APP_FILES/PYTHON/ObjectInfo/AStructure.py
@@ -27,49 +27,100 @@ class ObjectInfoS(QScrollArea):
                 self.Market()
         elif t == 'stock':
                 self.Stock()
-        print(t, i)
-    def Country(self):
-        pass
 
-    def Market(self):
+    def Country(self):
+#           --- Create objects ---
         self.WidgetW = QWidget(self)
         self.LayoutL = QGridLayout(self.WidgetW)
         self.IconL = QLabel(self.WidgetW)
-        self.TickerL
-        self.NameL
-        self.MicCodeL
-        self.OperatingMicL
-        self.CountryL
+        self.NameL = QLabel(self.WidgetW)
+        self.IsoL = QLabel(self.WidgetW)
+        self.ContinentL = QLabel(self.WidgetW)
+        self.SubRegion = QLabel(self.WidgetW)
+#           --- Info ---
+        self.InfoW = QWidget(self)
+        self.InfoL = QGridLayout(self.InfoW)
+        self.InfoTitleL = QLabel(self.InfoW)
+        self.CurrencyCodeNameL = QLabel(self.InfoW)
+        self.CurrencyCodeValueL = QLabel(self.InfoW)
+        self.CurrencyNameNameL = QLabel(self.InfoW)
+        self.CurrencyNameValueL = QLabel(self.InfoW)
+        self.CapitalCityNameL = QLabel(self.InfoW)
+        self.CapitalCityValueL = QLabel(self.InfoW)
+        self.TimezoneNameL = QLabel(self.InfoW)
+        self.TimezoneValueL = QLabel(self.InfoW)
+#           --- Stock Exchange Metadata ---
+        self.ExchangeNameNameL = QLabel(self.InfoW)
+        self.ExchangeNameValueL = QLabel(self.InfoW)
+        self.ExchangeCodeMicNameL = QLabel(self.InfoW)
+        self.ExchangeCodeMicValueL = QLabel(self.InfoW)
+        self.ExchangeCountryNameL = QLabel(self.InfoW)
+        self.ExchangeCountryValueL = QLabel(self.InfoW)
+        self.ExchangeTimezoneNameL = QLabel(self.InfoW)
+        self.ExchangeTimezoneValueL = QLabel(self.InfoW)
+        self.TradingHoursNameL = QLabel(self.InfoW)
+        self.TradingHoursValueL = QLabel(self.InfoW)
+        self.CurrencyNameL = QLabel(self.InfoW)
+        self.CurrencyValueL = QLabel(self.InfoW)
+#           --- Call functions ---
+        CountryUi(self)
+        CountryReloadStyle(self)
+        CountryRetranslate(self)
+#           --- Connect functions ---
 
-        self.InfoW
-        self.InfoL
-        self.InfoTitleL
-        self.CityNameL
-        self.CityValueL
-        self.TimezoneNameL
-        self.TimezoneValueL
-        self.CurrencyNameL
-        self.CurrencyValueL
-        self.WebsiteNameL
-        self.WebsiteValueL
-
-        self.TradingSessionW
-        self.TradingSessionL
-        self.TradingSessionTitleL
-        self.PreMarketNameL
-        self.PreMarketValueL
-        self.OpenNameL
-        self.OpenValueL
-        self.CloseNameL
-        self.CloseValueL
-
-        self.CalendarW
-        self.CalendarL
-        self.CalendarTitleL
-        self.FreeDaysNameL
-        self.FreeDaysValueL
-
-        self
+    def Market(self):
+#           --- Create objects ---
+        self.WidgetW = QWidget(self)
+        self.LayoutL = QGridLayout(self.WidgetW)
+        self.IconL = QLabel(self.WidgetW)
+        self.TickerL = QLabel(self.WidgetW)
+        self.NameL = QLabel(self.WidgetW)
+        self.MicCodeL = QLabel(self.WidgetW)
+        self.OperatingMicL = QLabel(self.WidgetW)
+        self.CountryL = QLabel(self.WidgetW)
+#           --- Info ---
+        self.InfoW = QWidget(self.WidgetW)
+        self.InfoL = QGridLayout(self.InfoW)
+        self.InfoTitleL = QLabel(self.InfoW)
+        self.FoundedYearNameL = QLabel(self.InfoW)
+        self.FoundedYearValueL = QLabel(self.InfoW)
+        self.CityNameL = QLabel(self.InfoW)
+        self.CityValueL = QLabel(self.InfoW)
+        self.TimezoneNameL = QLabel(self.InfoW)
+        self.TimezoneValueL = QLabel(self.InfoW)
+        self.CurrencyNameL = QLabel(self.InfoW)
+        self.CurrencyValueL = QLabel(self.InfoW)
+        self.WebsiteNameL = QLabel(self.InfoW)
+        self.WebsiteValueL = QLabel(self.InfoW)
+        self.MarketTypesNameL = QLabel(self.InfoW)
+        self.MarketTypesValueL = QLabel(self.InfoW)
+#           --- Trading session ---
+        self.TradingSessionTitleL = QLabel(self.InfoW)
+        self.PreMarketNameL = QLabel(self.InfoW)
+        self.PreMarketValueL = QLabel(self.InfoW)
+        self.OpenNameL = QLabel(self.InfoW)
+        self.OpenValueL = QLabel(self.InfoW)
+        self.CloseNameL = QLabel(self.InfoW)
+        self.CloseValueL = QLabel(self.InfoW)
+#           --- Calendar ---
+        self.CalendarTitleL = QLabel(self.InfoW)
+        self.FreeDaysNameL = QLabel(self.InfoW)
+        self.FreeDaysValueL = QLabel(self.InfoW)
+#           --- Exchange structure & regulation ---
+        self.ExchangeStructureTitleL = QLabel(self.InfoW)
+        self.InstrumentTypesNameL = QLabel(self.InfoW)
+        self.InstrumentTypesValueL = QLabel(self.InfoW)
+        self.MarketSegmentsNameL = QLabel(self.InfoW)
+        self.MarketSegmentsValueL = QLabel(self.InfoW)
+        self.RegulatorsNameL = QLabel(self.InfoW)
+        self.RegulatorsValueL = QLabel(self.InfoW)
+        self.TradingSystemsNameL = QLabel(self.InfoW)
+        self.TradingSystemsValueL = QLabel(self.InfoW)
+#           --- Call functions ---
+        MarketUi(self)
+        MarketReloadStyle(self)
+        MarketRetranslate(self)
+#           --- Connect functions ---
 
 
     def Stock(self):
@@ -96,23 +147,17 @@ class ObjectInfoS(QScrollArea):
         self.WebValueL = QLabel(self.InfoW)
         self.AdresNameL = QLabel(self.InfoW)
         self.AdresValueL = QLabel(self.InfoW)
-        self.ManagmentW = QWidget(self.WidgetW)
-        self.ManagmentL = QGridLayout(self.ManagmentW)
-        self.ManagmentTitleL = QLabel(self.ManagmentW)
-        self.CEONameL = QLabel(self.ManagmentW)
-        self.CEOValueL = QLabel(self.ManagmentW)
-        self.CFONameL = QLabel(self.ManagmentW)
-        self.CFOValueL = QLabel(self.ManagmentW)
-        self.ManagmentNameL = QLabel(self.ManagmentW)
-        self.ManagmentValueL = QLabel(self.ManagmentW)
-        self.SupervisoryBoardNameL = QLabel(self.ManagmentW)
-        self.SupervisoryBoardValueL = QLabel(self.ManagmentW)
+        self.ManagmentTitleL = QLabel(self.InfoW)
+        self.CEONameL = QLabel(self.InfoW)
+        self.CEOValueL = QLabel(self.InfoW)
+        self.CFONameL = QLabel(self.InfoW)
+        self.CFOValueL = QLabel(self.InfoW)
+        self.ManagmentNameL = QLabel(self.InfoW)
+        self.ManagmentValueL = QLabel(self.InfoW)
+        self.SupervisoryBoardNameL = QLabel(self.InfoW)
+        self.SupervisoryBoardValueL = QLabel(self.InfoW)
 #           --- Call functions ---
         StockUi(self)
         StockReloadStyle(self)
         StockRetranslate(self)
 #           --- Connect functions ---
-
-
-
-
